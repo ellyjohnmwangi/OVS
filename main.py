335b6846df9a30d81f177307f9bf54eedb4469a4
njoro <ellyjohnmwangi@gmail.com>
2023-10-05 21:39:35 +0300

    Voting logic created.

---
+++ main.py
@@ -0,0 +1,13 @@
+import http.server
+import socketserver
+# import SimpleHTTPServer
+
+
+ip_address = "127.0.0.1"
+port = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+http = socketserver.TCPServer((ip_address, port), Handler)
+print("Serving at port:", port)
+
+http.serve_forever()
+
