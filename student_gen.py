6f52c15c7c13464ed6aa8f25f92764914138b9df
njoro <ellyjohnmwangi@gmail.com>
2023-10-05 21:38:04 +0300

    Generates test students for the database

---
+++ student_gen.py
@@ -0,0 +1,22 @@
+import csv
+
+import faker
+
+fake = faker.Faker()
+
+csv_file = 'student_records.csv'
+num_records = 10
+
+with open(csv_file, mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['first_name', 'last_name', 'email', 'password'])
+
+    for _ in range(num_records):
+        first_name = fake.first_name()
+        last_name = fake.last_name()
+        email = fake.email()
+        password = 'password123'
+
+        writer.writerow([first_name, last_name, email, password])
+
+print(f"{num_records} sample student records generated and saved to '{csv_file}'.")
