2355d97d98b8168adebbe82ae860e8662b83d775
ellyjohnmwangi <ellyjohnmwangi@gmail.com>
2023-11-08 01:34:37 +0300

    Corrected pep 8 errors

--- handlers/home.py
+++ handlers/home.py
@@ -13,28 +13,28 @@ class Home:
         self.request_handler = request_handler
         # add logging
 
-    def HandleStudentHome(self):
+    def handle_student_home(self):
         self.request_handler.send_response(200)
         self.request_handler.send_header("Content-type", "text/html")
         self.request_handler.end_headers()
         with open("templates/student_home.html", "rb") as file:
             self.request_handler.wfile.write(file.read())
 
-    def HandleDelegate(self):
+    def handle_delegate(self):
         self.request_handler.send_response(200)
         self.request_handler.send_header("Content-type", "text/html")
         self.request_handler.end_headers()
         with open("templates/student_home.html", "rb") as file:
             self.request_handler.wfile.write(file.read())
 
-    def HandleAdmin(self):
+    def handle_admin(self):
         self.request_handler.send_response(200)
         self.request_handler.send_header("Content-type", "text/html")
         self.request_handler.end_headers()
         with open("templates/admin_home.html", "rb") as file:
             self.request_handler.wfile.write(file.read())
 
-    def HandlePollingOfficer(self):
+    def handle_polling_officer(self):
         self.request_handler.send_response(200)
         self.request_handler.send_header("Content-type", "text/html")
         self.request_handler.end_headers()
