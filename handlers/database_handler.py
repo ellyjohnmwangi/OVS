fea494e9daa1630fbe3ff30f5820ca0681ce3577
ellyjohnmwangi <johnmwangielly@gmail.com>
2023-11-17 22:56:17 +0300

    Added Candidate name from students database using student_id

--- handlers/database_handler.py
+++ handlers/database_handler.py
@@ -1,7 +1,6 @@
 import json
 from modules.db_connector import DBConnector
 
-
 class CandidateHandler:
     def __init__(self):
         self.db_connector = DBConnector()
@@ -10,7 +9,11 @@ class CandidateHandler:
         connection = self.db_connector.get_connection()
         cursor = connection.cursor()
 
-        select_query = "SELECT candidate_id, position_id, department FROM candidates"
+        select_query = """
+        SELECT c.candidate_id, s.first_name AS candidate_name, c.position_id, c.department
+        FROM candidates c
+        JOIN students s ON c.student_id = s.student_id;
+        """
         cursor.execute(select_query)
         candidate_data = cursor.fetchall()
 
@@ -18,8 +21,14 @@ class CandidateHandler:
         self.db_connector.close_connection()
 
         # Convert the data to a list of dictionaries
-        candidates = [{"candidate_id": candidate[0], "position_id": candidate[1], "department": candidate[2]} for
-                      candidate in candidate_data]
-
+        candidates = [
+            {
+                "candidate_id": candidate[0],
+                "candidate_name": candidate[1],
+                "position_id": candidate[2],
+                "department": candidate[3]
+            }
+            for candidate in candidate_data
+        ]
 
         return json.dumps(candidates)
