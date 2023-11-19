79d134dbef112f482e3ffba101c221e6cb75f4d9
ellyjohnmwangi <johnmwangielly@gmail.com>
2023-11-19 23:36:24 +0300

    Code clean up

--- handlers/voting.py
+++ handlers/voting.py
@@ -10,6 +10,7 @@ class VoteHandler:
     def __init__(self):
         self.db_connector = DBConnector()
         self.db_connection = self.db_connector.get_connection()
+
     def has_user_voted(self, user_id):
         try:
 
@@ -34,11 +35,11 @@ class VoteHandler:
             cursor.execute(insert_query, (student_id, candidate_id))
             self.db_connection.commit()
 
-    def handle_vote_count(self, student_id,  candidate_id):
+    def handle_vote_count(self, student_id, candidate_id):
         try:
             # Check if the user has already voted
             if self.has_user_voted(student_id):
-                return {"message": "You have already voted. Multiple votes are not allowed."}
+                return {"status": "error", "message": "You have already voted. Multiple votes are not allowed."}
 
             # Connect to the database and update the vote count
             #         connection = self.db_connector.get_connection()
@@ -53,35 +54,16 @@ class VoteHandler:
             # Insert the vote into the votes table to track the user's vote
             self.insert_vote(student_id, candidate_id)
 
-
-            return {"message": "Vote counted successfully"}
+            return {"status": "success", "message": "Vote counted successfully"}
         except IntegrityError as integrity_error:
             # Handle the case where the user has already voted
-            return {"message": "You have already voted. Multiple votes are not allowed."}
+            return {"status": "error", "message": "You have already voted. Multiple votes are not allowed."}
         except Exception as e:
             # Log other exceptions with details
             logging.error(e)
             logging.error(f"Error updating vote count: {str(e)}")
-            return {"message": f"Internal Server Error: {str(e)}"}
-    # def handle_vote_count(self, candidate_id):
-    #     try:
-    #         # Connect to the database and update the vote count
-    #         connection = self.db_connector.get_connection()
-    #         cursor = connection.cursor()
-    #
-    #         # Update the vote count for the specified candidate_id
-    #         update_query = "UPDATE candidates SET vote_count = vote_count + 1 WHERE candidate_id = %s"
-    #         cursor.execute(update_query, (candidate_id,))
-    #         print(update_query)
-    #         connection.commit()
-    #
-    #         cursor.close()
-    #         self.db_connector.close_connection()
-    #
-    #         return {"message": "Vote counted successfully"}
-    #     except Exception as e:
-    #         # Handle exceptions, log errors, etc.
-    #         return {"message": f"Error counting vote: {str(e)}"}
+            return {"status": "error", "message": f"Internal Server Error: {str(e)}"}
+
 
     def get_response(self, result_message):
         # Convert the result_message dictionary to a JSON-encoded string
