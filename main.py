ea28f2e48439ad37807f8050a0c4a61b8f205bef
ellyjohnmwangi <johnmwangielly@gmail.com>
2023-11-19 21:38:23 +0300

    Code cleanup

--- main.py
+++ main.py
@@ -12,14 +12,11 @@ import sys
 
 from handlers import router
 
-# Initiate internal libraries for importing get_db_connection
-# path = os.path.abspath("./modules")
-# sys.path.append(path)
-
 path = os.path.abspath("./handlers")
 sys.path.append(path)
 PORT = 8000
 IsVottingOn = False
+
 
 class MyTCPServer(socketserver.TCPServer):
     allow_reuse_address = True
@@ -34,24 +31,3 @@ def run_server():
 
 if __name__ == "__main__":
     run_server()
-
-"""
-def main():
-    db = DBConnector(
-        host="localhost",
-        user="root",
-        password="",
-        database="ovs_student"
-    )
-
-    cnct = db.get_connection()
-    if not cnct:
-        print("Error connecting to DB, Check Logs")
-
-    db.close_connection()
-
-if __name__ == "__main__":
-    main()
-"""
-import logging
-import mysql.connector
