559c79fb6d8bd4031dc4d148a819094a8d21c849
ellyjohnmwangi <johnmwangielly@gmail.com>
2023-11-19 21:42:55 +0300

    Update Voting handler class to add update vote table functionality and fixed how the db connection works

--- handlers/voting.py
+++ handlers/voting.py
@@ -1,89 +1,87 @@
-import hashlib
 import json
+import logging
+
+from mysql.connector import IntegrityError
 
 from modules.db_connector import DBConnector
-
-
-class VotingHandler:
-    def __init__(self):
-        # Create a DBConnector instance
-        self.db_connector = DBConnector()
-
-    def handle_vote(self, student_id, term_id, candidate_id, position_id):
-        try:
-            # Get the database connection
-            connection = self.db_connector.get_connection()
-            cursor = connection.cursor()
-
-            # Check if the user has already voted
-            check_vote_query = """
-                SELECT voting_id
-                FROM voting
-                WHERE student_id = %s AND term_id = %s AND position_id = %s
-            """
-            cursor.execute(check_vote_query, (student_id, term_id, position_id))
-            existing_vote = cursor.fetchone()
-
-            if existing_vote:
-                # User has already voted, handle accordingly
-                return "You have already voted. Thank you!"
-
-            # Update the candidate's vote count
-            update_vote_count_query = """
-                UPDATE candidates
-                SET no_of_votes = no_of_votes + 1
-                WHERE candidate_id = %s
-            """
-            cursor.execute(update_vote_count_query, (candidate_id,))
-            connection.commit()
-
-            # Generate a hash for auditing and verification
-            vote_data = f"{student_id}:{term_id}:{candidate_id}:{position_id}".encode()
-            hash_value = hashlib.sha256(vote_data).hexdigest()
-
-            # Store the vote details in the voting table for auditing
-            store_vote_query = """
-                INSERT INTO voting (student_id, term_id, candidate_id, position_id, hash)
-                VALUES (%s, %s, %s, %s, %s)
-            """
-            cursor.execute(store_vote_query, (student_id, term_id, candidate_id, position_id, hash_value))
-            connection.commit()
-
-            return "Your vote has been recorded. Thank you for voting!"
-
-        except Exception as err:
-            print(f"Error handling vote: {err}")
-            return "An error occurred while processing your vote. Please try again later."
-
-        finally:
-            # Close the database connection
-            cursor.close()
-            self.db_connector.close_connection()
 
 
 class VoteHandler:
     def __init__(self):
         self.db_connector = DBConnector()
-
-    def handle_vote_count(self, candidate_id):
+        self.db_connection = self.db_connector.get_connection()
+    def has_user_voted(self, user_id):
         try:
+
+            with self.db_connection.cursor() as cursor:
+                select_query = "SELECT * FROM votes WHERE student_id = %s"
+                cursor.execute(select_query, (user_id,))
+                result = cursor.fetchone()
+                cursor.close()
+                logging.info(result)
+        except Exception as e:
+            # Log other exceptions with details
+            logging.error(e)
+            logging.error(f"Error updating vote count: {str(e)}")
+            return {"message": f"Internal Server Error: {str(e)}"}
+
+        return bool(result)
+
+    def insert_vote(self, student_id, candidate_id):
+        with self.db_connection.cursor() as cursor:
+            # Insert the vote into the votes table
+            insert_query = "INSERT INTO votes (student_id, candidate_id) VALUES (%s, %s)"
+            cursor.execute(insert_query, (student_id, candidate_id))
+            self.db_connection.commit()
+
+    def handle_vote_count(self, student_id,  candidate_id):
+        try:
+            # Check if the user has already voted
+            if self.has_user_voted(student_id):
+                return {"message": "You have already voted. Multiple votes are not allowed."}
+
             # Connect to the database and update the vote count
-            connection = self.db_connector.get_connection()
-            cursor = connection.cursor()
+            #         connection = self.db_connector.get_connection()
+            #         cursor = connection.cursor()
+            # Connect to the database and update the vote count
+            with self.db_connection.cursor() as cursor:
+                # Update the vote count for the specified candidate_id
+                update_query = "UPDATE candidates SET vote_count = vote_count + 1 WHERE candidate_id = %s"
+                cursor.execute(update_query, (candidate_id,))
+                self.db_connection.commit()
 
-            # Update the vote count for the specified candidate_id
-            update_query = "UPDATE candidates SET vote_count = vote_count + 1 WHERE candidate_id = %s"
-            cursor.execute(update_query, (candidate_id,))
-            print(update_query)
-            connection.commit()
+            # Insert the vote into the votes table to track the user's vote
+            self.insert_vote(student_id, candidate_id)
 
-            cursor.close()
-            self.db_connector.close_connection()
 
             return {"message": "Vote counted successfully"}
+        except IntegrityError as integrity_error:
+            # Handle the case where the user has already voted
+            return {"message": "You have already voted. Multiple votes are not allowed."}
         except Exception as e:
-            # Handle exceptions, log errors, etc.
-            return {"message": f"Error counting vote: {str(e)}"}
+            # Log other exceptions with details
+            logging.error(e)
+            logging.error(f"Error updating vote count: {str(e)}")
+            return {"message": f"Internal Server Error: {str(e)}"}
+    # def handle_vote_count(self, candidate_id):
+    #     try:
+    #         # Connect to the database and update the vote count
+    #         connection = self.db_connector.get_connection()
+    #         cursor = connection.cursor()
+    #
+    #         # Update the vote count for the specified candidate_id
+    #         update_query = "UPDATE candidates SET vote_count = vote_count + 1 WHERE candidate_id = %s"
+    #         cursor.execute(update_query, (candidate_id,))
+    #         print(update_query)
+    #         connection.commit()
+    #
+    #         cursor.close()
+    #         self.db_connector.close_connection()
+    #
+    #         return {"message": "Vote counted successfully"}
+    #     except Exception as e:
+    #         # Handle exceptions, log errors, etc.
+    #         return {"message": f"Error counting vote: {str(e)}"}
 
     def get_response(self, result_message):
         # Convert the result_message dictionary to a JSON-encoded string
